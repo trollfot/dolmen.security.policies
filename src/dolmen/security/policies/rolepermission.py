ac03288319a48646ebfcbadda242d37e10f0fb06
Christian Klinger <cklinger@novareto.de>
2011-02-21 13:59:51 +0100

    more changes

--- src/dolmen/security/policies/rolepermission.py
+++ src/dolmen/security/policies/rolepermission.py
@@ -12,5 +12,34 @@ class ExtraRolePermissionMap(RolePermissionManager):
         self.context = context
         self.extra = self._compute_extra_data()
 
-    def getRolesAndPermissions(self):
-        return self.getAllCells() + self.extra.getAllCells()
+    def getAllCells(self):
+        res = []
+        for r in self._byrow.keys():
+            for c in self._byrow[r].items():
+                res.append((r,) + c)
+        res.extend(self.extra.getAllCells())
+        return res
+
+    def queryCell(self, rowentry, colentry, default=None):
+        cell = self.extra.queryCell(rowentry, colentry, default=default)
+        if cell is default:
+            return AnnotationPrincipalRoleManager.queryCell(
+                self, rowentry, colentry, default=default)
+        return cell
+
+    def getCol(self, colentry):
+        col = self.extra._bycol.get(colentry)
+        former = self._bycol.get(colentry)
+        if former:
+            col.update(former)
+        if col:
+            return col.items()
+        return []
+
+    def getRow(self, rowentry):
+        row = self.extra._byrow.get(rowentry)
+        row.extend(self._byrow.get(rowentry))
+        if row:
+            return row.items()
+        return []
+
